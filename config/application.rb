1d059ef73c33b83dc6fed7d4f348c7737fea3cfc
sakurannn <matakisakura@matakisakuranoMacBook-puro.local>
2019-02-10 20:46:27 +0900

    deviseの設定

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module ChatSpace
@@ -13,9 +11,7 @@ module ChatSpace
       g.javascripts false
       g.helper false
       g.test_framework false
+      config.i18n.default_locale = :ja
     end
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
   end
 end
