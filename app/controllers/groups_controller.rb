1d1089df374e0f86784180fc74e8330b0533b0ab
sakurannn <matakisakura@matakisakuranoMacBook-puro.local>
2019-02-11 21:24:03 +0900

    グループ作成機能のアクションの定義

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,17 @@
 class GroupsController < ApplicationController
 
   def new
+    @group = Group.new
+    @group.users << current_user
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+      ens
   end
 
   def edit
@@ -12,5 +20,8 @@ class GroupsController < ApplicationController
   def update
   end
 
-
+  private
+  def group_params
+    params.require(:group).permit(:name, { :user_ids => []})
+  end
 end
