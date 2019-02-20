f367382b9ae7a618cc8cc118212eb55785c30b2d
sakurannn <matakisakura@matakisakuranoMacBook-puro.local>
2019-02-20 16:20:42 +0900

    フォームのリセット記述変更

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -38,7 +38,7 @@ $(function(){
       var html = buildHTML(data);
       $('.messages').append(html);
       $('.messages').animate({scrollTop:$('.messages')[0].scrollHeight}, 'fast');
-      $('.form__message').val('');
+      $('form')[0].reset();
       $('.form__submit').attr("disabled",false);
     })
     .fail(function(){
